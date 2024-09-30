11a3a47ac134113ba2597a7f80a6b014274bd900
Johnny-Zhao-ICD2O1-01 <johnny.zhao@student.tdsb.on.ca>
2024-09-30 13:56:52 +0000

    commit

--- PP1_9.py
+++ PP1_9.py
@@ -1,31 +1,36 @@
-
+'''
+    Practice Questions: String Manipulation
+    Author: Johnny Zhao
+    Date Created: Sept 30, 2024
+    Date Last Modified: Sept 30, 2024
+'''
 
 def q1(): 
-  #Write Assignment code here
-
+  print('"Hello World"') #printing "Hello World" with the quotations using single and double quotations
 
 def q2(): 
-  #Write Assignment code here
-
+  wordQ2 = input("Input a word: ") #asking the user to input a word
+  print(wordQ2.lower()) #outputting the result in all lowercase letters
+  print(wordQ2.upper()) #outputting the result in all uppercase letters
 
 def q3(): 
-  #Write Assignment code here
-
+  wordQ3 = input("Input a word that is at least 5 letters long: ") #asking the user to input a word that is at least 5 letters long
+  print(wordQ3[1:4]) #outputting the 2nd to 4th letter of the result
 
 def q4(): 
-  #Write Assignment code here
-
+  wordQ4 = input("Input a word: ") #asking the user to input a word
+  print(wordQ4.index("o")) #outputting the index where the first "o" is in the result
 
 def q5(): 
-  #Write Assignment code here
-
-
+  wordQ5 = input("Input a word: ") #asking the user to input a word
+  print(len(wordQ5)) #outputting the length of the result
 
 #Do not alter the following code
 #Comment out the following code when running your tests
-
+'''
 q1()
 q2()
 q3()
 q4()
 q5()
+'''
